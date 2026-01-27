0ec15c773a8f704411fc3906f5226a1e09f5aadb
_mqt. <23133071@student.hcmute.edu.vn>
2026-01-27 22:24:08 +0700

    Update MAFC_module.py

--- model/MAFC_module.py
+++ model/MAFC_module.py
@@ -106,7 +106,8 @@ class BAM(nn.Module):
         if not no_spatial:
             self.SpatialGate = SpatialGate_new1(gate_channels)
     def forward(self, x):
-        x_out = self.ChannelGate(x)
-        if not self.no_spatial:
-            x_out = self.SpatialGate(x_out)
-        return x_out
+    ch_att = self.ChannelGate(x)
+    if not self.no_spatial:
+        sp_att = self.SpatialGate(x)   # dùng x gốc
+        return ch_att * sp_att
+    return ch_att
