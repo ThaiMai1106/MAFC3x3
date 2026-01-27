53823713b0dde20fd61b058185e3e8c53219ee60
_mqt. <23133071@student.hcmute.edu.vn>
2026-01-27 22:04:45 +0700

    Update MAFC_module.py

--- model/MAFC_module.py
+++ model/MAFC_module.py
@@ -2,27 +2,79 @@ import torch
 import math
 import torch.nn as nn
 import torch.nn.functional as F
+from .common import get_activation
+
+class BasicConv(nn.Module):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False, activation='relu'):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = get_activation(activation) if relu else None
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
 
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
-class ChannelGate(nn.Module):
-    def __init__(self, gate_channel, reduction_ratio=16, num_layers=1):
-        super(ChannelGate, self).__init__()
-        self.gate_c = nn.Sequential()
-        self.gate_c.add_module( 'flatten', Flatten() )
-        gate_channels = [gate_channel]
-        gate_channels += [gate_channel // reduction_ratio] * num_layers
-        gate_channels += [gate_channel]
-        for i in range( len(gate_channels) - 2 ):
-            self.gate_c.add_module( 'gate_c_fc_%d'%i, nn.Linear(gate_channels[i], gate_channels[i+1]) )
-            self.gate_c.add_module( 'gate_c_bn_%d'%(i+1), nn.BatchNorm1d(gate_channels[i+1]) )
-            self.gate_c.add_module( 'gate_c_relu_%d'%(i+1), nn.ReLU() )
-        self.gate_c.add_module( 'gate_c_fc_final', nn.Linear(gate_channels[-2], gate_channels[-1]) )
-    def forward(self, in_tensor):
-        avg_pool = F.avg_pool2d( in_tensor, in_tensor.size(2), stride=in_tensor.size(2) )
-        return self.gate_c( avg_pool ).unsqueeze(2).unsqueeze(3).expand_as(in_tensor)
 
+class ChannelGate(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=None,activation='relu',excite_activation="sigmoid"):    
+        super(ChannelGate, self).__init__()
+        self.gate_channels = gate_channels
+        self.activation1 = get_activation(activation)
+        self.activation2 = get_activation(excite_activation)
+        if gate_channels // reduction_ratio == 0: #fixed for mobileNetV2
+            reduction_ratio = gate_channels
+        self.mlp = nn.Sequential(
+            Flatten(),
+            nn.Linear(len(pool_types)*gate_channels, gate_channels // reduction_ratio),            
+            self.activation1,
+            nn.Linear(gate_channels // reduction_ratio, gate_channels)
+            )
+        self.pool_types = pool_types        
+    def forward(self, x):
+        channel_att_sum = None
+        squeeze_all=None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                squeeze = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
+            elif pool_type=='max':
+                squeeze = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))                
+            elif pool_type=='std':                
+                stdf = torch.std(x,(2,3),unbiased=True)#compute standard deviation
+                squeeze = stdf.reshape(stdf.size()[0],stdf.size()[1],1,1)#resize to be (,1,1) the same as out put of AdaptiveAvgPool2d , i.e., self.squeeze(residual)                
+            if squeeze_all is None:
+                squeeze_all = squeeze
+            else:
+                squeeze_all = torch.cat((squeeze_all,squeeze),1)                
+        channel_att_sum = self.mlp(squeeze_all)    
+        scale = self.activation2(channel_att_sum).unsqueeze(2).unsqueeze(3).expand_as(x)
+        return x * scale
+
+class ChannelPool(nn.Module):
+    def __init__(self, pool_types=None):    
+        super(ChannelPool, self).__init__()
+        self.pool_types = pool_types
+    def forward(self, x):
+        spa_all = None
+        for pool_type in self.pool_types:
+            if pool_type=='avg':
+                spa1 = torch.mean(x,1).unsqueeze(1)                
+            if pool_type=='max':
+                spa1 = torch.max(x,1)[0].unsqueeze(1)
+            if pool_type=='std':
+                spa1 = torch.std(x,1).unsqueeze(1)
+            if spa_all is None:
+                spa_all = spa1
+            else:
+                spa_all = torch.cat((spa_all, spa1), dim=1)
+        return spa_all
 class SpatialGate_new1(nn.Module):
     def __init__(self, gate_channel, reduction_ratio=8):
         super(SpatialGate_new1, self).__init__()
@@ -42,17 +94,19 @@ class SpatialGate_new1(nn.Module):
         self.gate_s.add_module('gate_s_relu0',nn.ReLU())
         self.gate_s.add_module('gate_s_conv_reduce', nn.Conv2d(cout, 1, kernel_size=1))
 
-
-
-
     def forward(self, in_tensor):
         x_gate_s = self.gate_s( in_tensor )
         return x_gate_s
-class BAM(nn.Module):
-    def __init__(self, gate_channel):
+
+class BAMC(nn.Module):
+    def __init__(self, gate_channels, reduction_ratio=16, pool_types=None, no_spatial=False,activation='relu',excite_activation="sigmoid"):
         super(BAM, self).__init__()
-        self.channel_att = ChannelGate(gate_channel)
-        self.spatial_att = SpatialGate_new1(gate_channel)
-    def forward(self,in_tensor):
-        att = 1 + torch.sigmoid( self.channel_att(in_tensor) * self.spatial_att(in_tensor) )
-        return att * in_tensor
+        self.ChannelGate = ChannelGate(gate_channels, reduction_ratio, pool_types,activation=activation,excite_activation=excite_activation)
+        self.no_spatial=no_spatial
+        if not no_spatial:
+            self.SpatialGate = SpatialGate_new1(gate_channels)
+    def forward(self, x):
+        x_out = self.ChannelGate(x)
+        if not self.no_spatial:
+            x_out = self.SpatialGate(x_out)
+        return x_out
